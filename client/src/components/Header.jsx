cfa3b94357c83eb0435099185cd41ef581d22cfc
Jacob Erholt <jacob@erholt.dk>
2026-08-11 13:36:12 +0200

    make header have an height

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import ProfileBadge from './Layout/Header/ProfileBadge';
 
 const Header = () => {
   return (
-    <header className="flex px-4 sticky z-50" style={{top: 0}}>
+    <header className="flex h-16 px-4 sticky z-50" style={{top: 0}}>
       <a href="/" title="Logo of the page" id="logo" className="p-1 m-2 text-4xl"><FaDice /></a>
       <ul className="flex items-center grow justify-items-end list-none justify-end">
         <ColorTheme />
